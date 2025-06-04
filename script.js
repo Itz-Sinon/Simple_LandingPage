10b239f3852036d901635776d76b458886850e4c
ItzSinon <ttlhnmh@gmail.com>
2025-06-04 11:30:21 +0700

    new menu mobile + change text center + remove margin body

---
+++ script.js
@@ -0,0 +1,8 @@
+function Menu() {
+  var x = document.getElementById("Links");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
