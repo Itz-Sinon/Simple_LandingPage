70e0c3f31fe1e3273286fcd2371398194cd45354
ItzSinon <ttlhnmh@gmail.com>
2025-06-04 15:12:10 +0700

    fix menu mobile part1

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
+var MenuBtn = document.getElementById("MenuBtn");
+var x = document.getElementById("Links");
+
 function Menu() {
-  var x = document.getElementById("Links");
   if (x.style.display === "block") {
     x.style.display = "none";
   } else {
     x.style.display = "block";
   }
 }
+
+function handleResize() {
+  if (window.innerWidth > 780) {
+    x.style.display = "";
+  }
+}
+window.addEventListener("resize", handleResize);
